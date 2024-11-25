ea13e86f47352ec91b8d21b710682e01c950c7a5
Joffrey Trn <101974770+MentalDeFeur@users.noreply.github.com>
2024-11-25 08:24:14 +0100

    ajouttouchdrag

--- Character.gd
+++ Character.gd
@@ -2,30 +2,28 @@ extends CharacterBody2D
 
 
 const JUMP_VELOCITY = 400
+var DRAG_SPEED = 1000
 @onready var animate = $Personnage
-var start_drag_position = Vector2(0,0)
+var start_drag_position = Vector2()
 var is_dragging=false
 var touch_pos=0
 
 func _ready():
 	pass
-	
-func _input(event: InputEvent):
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			is_dragging=true
-		else:
-			is_dragging=false
-	if is_dragging:
+
+func _input(event):
+	if event is InputEventScreenDrag:
+		is_dragging=true
 		touch_pos = event.position
 		
 func _physics_process(delta: float) -> void:
 	velocity.y = JUMP_VELOCITY
-	
 	if velocity.length() > 0:
 		velocity = velocity.move_toward(Vector2.ZERO,delta)
-	
+		
 	if is_dragging:
-		$Personnage.global_position = touch_pos
-		move_and_slide()
+		var direction = (touch_pos - global_position).normalized()
+		velocity.x = direction.x * DRAG_SPEED
+	
+	move_and_slide()
 		
