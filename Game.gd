2441270f5f7c6748d07484042296aa8e24640b0b
Joffrey Trn <101974770+MentalDeFeur@users.noreply.github.com>
2025-08-10 20:23:40 +0200

    Update Game.gd

--- Game.gd
+++ Game.gd
@@ -3,7 +3,10 @@ extends Node
 var spawn_interval : float = 1.3
 var spawn_timer = 0
 var value = Global.value
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 48fba2d (correctionbug)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MusicAttrap.play()
@@ -19,11 +22,20 @@ func _process(delta):
 		
 func spawn_apple():
 	var apple = $NodeApple/Area2D/Apple
+<<<<<<< HEAD
+=======
+	$NodeApple/Area2D/Apple/AnimatedSprite2D.play("apple")
+>>>>>>> parent of 48fba2d (correctionbug)
 	var random_x = randf_range(0,648)
 	apple.position = Vector2(random_x, 0)
 	add_child(apple)
 	
 func _on_area_character_body_entered(body: Node2D) -> void:
+<<<<<<< HEAD
+=======
+		$AppleCollected.play()
+		$NodeApple/Area2D/Apple/AnimatedSprite2D.play("collected")
+>>>>>>> parent of 48fba2d (correctionbug)
 		value += 1
 		$Score.text = "Score : {value}".format({"value": value})
 		
